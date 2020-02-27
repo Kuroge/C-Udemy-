04e8710d6158817c939dc052a8c34a9fe8b06f3d
Adrián García Riera <agriera@stdmultiopcion.es>
2020-02-27 13:17:07 +0100

    Added Files exercises

---
+++ CSharp - Udemy Beginner/Exercise 1 - Files/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_1___Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = File.ReadAllText(@"C:\temp\CSharpUdemy.txt");
+            Console.WriteLine("File contains {0} words", txt.Split(" ").Length);
+
+        }
+    }
+}
---
+++ CSharp - Udemy Beginner/Exercise 2 - Files/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_2___Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var longestWord = "";
+            foreach (var word in File.ReadAllText(@"C:\temp\CSharpUdemy.txt").Split(" "))
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
